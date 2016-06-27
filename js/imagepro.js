2946896407b9e5b22f1581fff5629e8aaae1289d
Sushant Gautam <susant.gautam@gmail.com>
2016-06-27 17:11:40 +0545

    Image merging script added

---
+++ js/imagepro.js
@@ -0,0 +1,37 @@
+function after(abc,  def){
+    var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+var imga = loadImage(abc, mainI);
+var img2 = loadImage(def, mainI);
+var imagesLoaded = 0;
+
+function mainI() {
+    imagesLoaded += 1;
+
+    if(imagesLoaded == 2) {
+        // composite now
+
+
+        ctx.drawImage(imga, 0, 0);
+        
+        ctx.globalAlpha = 0.5;
+        ctx.drawImage(img2, 0, 0);
+
+        
+    }
+}
+
+function loadImage(src, onload) {
+    // http://www.thefutureoftheweb.com/blog/image-onload-isnt-being-called
+    var img = new Image();
+    
+    img.onload = onload;
+    img.src = src;
+
+    img.style.width = '200px';
+
+    return img;
+}
+
+};
